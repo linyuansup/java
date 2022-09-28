f9014dc15f5a3f514333e72f1b4ca2a8d7309203
linyuansup <linyuansup@gmail.com>
2022-09-28 23:47:34 +0800

    重构客户端UI

--- src/sy/b/client/Client.java
+++ src/sy/b/client/Client.java
@@ -1,4 +1,37 @@
 package sy.b.client;
 
+import sy.b.ui.BottomBar;
+import sy.b.ui.ClientTopBar;
+import sy.b.ui.MiddleBar;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class Client {
+    public static void main(String[] args) {
+        JFrame mainUI = new JFrame();
+        ClientTopBar clientTopBar = new ClientTopBar(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        MiddleBar middleBar = new MiddleBar();
+        BottomBar bottomBar = new BottomBar(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        mainUI.setLayout(new BorderLayout());
+        mainUI.setTitle("客户端");
+        mainUI.setSize(500,500);
+        mainUI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainUI.add(clientTopBar, BorderLayout.NORTH);
+        mainUI.add(middleBar, BorderLayout.CENTER);
+        mainUI.add(bottomBar, BorderLayout.SOUTH);
+        mainUI.setVisible(true);
+    }
 }
--- src/sy/b/ui/ClientTopBar.java
+++ src/sy/b/ui/ClientTopBar.java
@@ -10,8 +10,7 @@ public class ClientTopBar extends JPanel {
     private final JLabel portLabel;
     private final JTextArea portArea;
     private final JButton connectButton;
-    private final JPanel IPLayout;
-    private final JPanel portLayout;
+    private final JPanel RightLayout;
 
     public ClientTopBar(ActionListener onClick) {
         super();
@@ -20,18 +19,16 @@ public class ClientTopBar extends JPanel {
         portLabel = new JLabel("端口号：");
         portArea = new JTextArea("54531", 1, 5);
         connectButton = new JButton("连接");
-        IPLayout = new JPanel(new BorderLayout(5,5));
-        portLayout = new JPanel(new BorderLayout(5,5));
+        RightLayout = new JPanel(new BorderLayout());
         connectButton.addActionListener(onClick);
+        RightLayout.add(portLabel, BorderLayout.WEST);
+        RightLayout.add(portArea, BorderLayout.CENTER);
+        RightLayout.add(connectButton, BorderLayout.EAST);
         setBorder(BorderFactory.createTitledBorder("客户端设置"));
-        setLayout(new BorderLayout(5,5));
-        portLayout.add(portLabel, BorderLayout.WEST);
-        portLayout.add(portLabel, BorderLayout.EAST);
-        IPLayout.add(IPLabel, BorderLayout.WEST);
-        IPLayout.add(IPArea, BorderLayout.EAST);
-        add(IPLayout, BorderLayout.WEST);
-        add(portLayout, BorderLayout.CENTER);
-        add(connectButton, BorderLayout.EAST);
+        setLayout(new BorderLayout(5, 5));
+        add(IPLabel, BorderLayout.WEST);
+        add(IPArea, BorderLayout.CENTER);
+        add(RightLayout, BorderLayout.EAST);
     }
 
     public int getPort() {
