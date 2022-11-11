2478a99f2b63b5eefa67e3af1fd1cdef81bc41f1
linyuansup <linyuansup@gmail.com>
2022-11-11 18:09:40 +0800

    补第一次作业

---
+++ src/sy/a/a/A.java
@@ -0,0 +1,8 @@
+package sy.a.a;
+
+// cd src
+// javac .\sy\a\a\A.java
+
+public class A {
+    
+}
---
+++ src/sy/a/b/B.java
@@ -0,0 +1,12 @@
+package sy.a.b;
+
+// cd src
+// javac .\sy\a\b\B.java
+
+import sy.a.a.A;
+
+public class B {
+    public static void main(String[] args) {
+        A a = new A();
+    }
+}
